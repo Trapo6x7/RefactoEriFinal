7b5a743c8480b1adccb74b28357b7392555617e9
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-04-30 10:13:19 +0200

    DONE : ADMIN

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -6,19 +6,52 @@
     $user = backpack_user();
 @endphp
 
-@if ($user && $user->role === 'superadmin')
-    <x-backpack::menu-item title="Users"  :link="backpack_url('user')" />
-    <x-backpack::menu-item title="Techniciens" :link="backpack_url('tech')" />
-    <x-backpack::menu-item title="Statuts" :link="backpack_url('problem-status')" />
-@endif
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="usersDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Interne
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="internDropdown">
+        <li>
+            <x-backpack::menu-item title="Users" :link="backpack_url('user')" class="dropdown-item" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Techniciens" :link="backpack_url('tech')" class="dropdown-item" />
+        </li>
+    </ul>
+</li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="usersDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      Externe
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="externDropdown">
+        <li>
+            <x-backpack::menu-item title="Societés" :link="backpack_url('society')" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Interlocuteurs" :link="backpack_url('interlocutor')" />        </li>
+    </ul>
+</li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="usersDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      Maintenance
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="maintenanceDropdown">
+        <li>
+            <x-backpack::menu-item title="Statuts" :link="backpack_url('problem-status')" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Problèmes" :link="backpack_url('problem')" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Environnements" :link="backpack_url('env')" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Outils" :link="backpack_url('tool')" />
+        </li>
+    </ul>
+</li>
 
 
-<x-backpack::menu-item title="Societés" :link="backpack_url('society')" />
-<x-backpack::menu-item title="Interlocuteurs" :link="backpack_url('interlocutor')" />
-<x-backpack::menu-item title="Problèmes" :link="backpack_url('problem')" />
-<x-backpack::menu-item title="Environnements" :link="backpack_url('env')" />
-<x-backpack::menu-item title="Outils" :link="backpack_url('tool')" />
-
-@if ($user && $user->role === 'superadmin')
-    <x-backpack::menu-item title="Menus" :link="backpack_url('menu')" />
-@endif
+<x-backpack::menu-item title="Raccourci" :link="backpack_url('menu')" />
